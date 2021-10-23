0194fb89fdfc241cd6d25555aa8e90e66e66be40
MikiEremiki <mr.eremin.ma@ya.ru>
2021-10-23 19:57:58 +0300

    Изучил, специальный RegexHandler.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import logging
 
 from telegram.ext import (Updater, Filters,
-                          CommandHandler, MessageHandler)
+                          CommandHandler, MessageHandler,
+                          RegexHandler, ConversationHandler)
 
 import settings
 from handlers import (start, guess_number, talk_to_me, send_cat_picture,
                       user_coordinates, save_user_photo, test)
-
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -20,15 +20,14 @@ def create_dp(bot):
     dp.add_handler(CommandHandler('test', test))
     dp.add_handler(CommandHandler('guess', guess_number))
     dp.add_handler(CommandHandler('cat', send_cat_picture))
-    dp.add_handler(MessageHandler(Filters.regex('^(Прислать котика)$'),
-                                  send_cat_picture))
+    dp.add_handler(RegexHandler('^(Прислать котика)$', send_cat_picture))
     dp.add_handler(MessageHandler(Filters.location, user_coordinates))
     dp.add_handler(MessageHandler(Filters.contact, test))
     dp.add_handler(MessageHandler(Filters.photo, save_user_photo))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
     mybot = Updater(settings.API_KEY)
 
     logging.info('Бот запущен')
