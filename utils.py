fbccbf742b088f74c48dd1ffec9b6bd78fdf4bf8
MikiEremiki <mr.eremin.ma@ya.ru>
2021-10-23 20:52:03 +0300

    Добавил возможность сменить автарку

--- utils.py
+++ utils.py
@@ -18,18 +18,19 @@ def main_keyboard():
     my_location = KeyboardButton('Мои координаты', request_location=True)
     return ReplyKeyboardMarkup([
         [my_contact, my_location],
-        ['Прислать котика', 'Заполнить анкету']
+        ['Прислать котика', 'Сменить аватар', 'Заполнить анкету']
     ], resize_keyboard=True)
 
 
 def play_random_numbers(user_number, context):
+    emo = context.user_data['emoji']
     bot_number = randint(user_number-10, user_number+10)
     if user_number > bot_number:
-        message = f'Ты {context.user_data["emoji"]} загадал {user_number}, я загадал {bot_number}, ' \
+        message = f'Ты {emo} загадал {user_number}, я загадал {bot_number}, ' \
                   f'ты выиграл!'
     elif user_number == bot_number:
-        message = f'Ты {context.user_data["emoji"]} загадал {user_number}, я загадал {bot_number}, ничья!'
+        message = f'Ты {emo} загадал {user_number}, я загадал {bot_number}, ничья!'
     else:
-        message = f'Ты {context.user_data["emoji"]} загадал {user_number}, я загадал {bot_number}, ' \
+        message = f'Ты {emo} загадал {user_number}, я загадал {bot_number}, ' \
                   f'я выиграл!'
     return message
